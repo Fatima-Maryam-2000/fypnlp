e072f925ca3443effa0b2fd6ce25eb7ef5dc128e
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-01-21 03:57:20 +0500

    data overflow crashing fixed

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 def name(user_id):
     # validate user id
     if (validate(user_id=user_id)):
-        text_a = request.args.get("text_a")
-        text_b = request.args.get("text_b")
+        # in last approach we sent data as query parameter with ?text_a=something&text_b=something
+        # as you can see below next two commented lines
+        # text_a = request.args.get("text_a")
+        # text_b = request.args.get("text_b")
+        # now we're sending data as body. so now we select body in thunder client rather than header.
+        # header dont support sending of large data body does.
+        request_data = request.get_json()
+        text_a = request_data["text_a"]
+        text_b = request_data["text_b"]
+        print(text_a)
+        print(text_b)
         return str(similarityCheck(text_a, text_b))
     return "Invalid User, Event will be logged"
 
@@ -20,12 +29,12 @@ def validate(user_id):
         print("User " + str(user_id) + " authentication")
         return True
     print("User " + str(user_id) + " unauthorized")
-    logger("Log this bhaiya g 😁")
+    logger("Log this bhaiya g")
     return False
 
 
 def similarityCheck(ideaA, ideaB):
-    # nlp algorithm here 😃
+    # nlp algorithm here
     # store similarity check score in 'score variable here'
     score = 97.9
     return score
@@ -37,4 +46,4 @@ def logger(loggedText):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port="3000")
+    app.run(debug=True, host="127.0.0.1", port="5000")
